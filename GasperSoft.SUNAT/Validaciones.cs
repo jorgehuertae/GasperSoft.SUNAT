7c06cfade5d54d0e51dc4da3038064d9c49e71f0
Jhon Larry Velezmoro Soza <it@gaspersoft.com>
2025-01-05 23:05:40 -0500

    IsValidCodigoInformacionAdicional hasta 36 caracteres

--- GasperSoft.SUNAT/Validaciones.cs
+++ GasperSoft.SUNAT/Validaciones.cs
@@ -437,7 +437,7 @@ namespace GasperSoft.SUNAT
         /// <param name="input">La cadena a evaluar</param>
         public static bool IsValidCodigoInformacionAdicional(string input)
         {
-            var regex = new Regex(@"^[0-9a-zA-Z_]{2,30}$");
+            var regex = new Regex(@"^[0-9a-zA-Z_]{2,36}$");
 
             return regex.IsMatch(input ?? "");
         }
